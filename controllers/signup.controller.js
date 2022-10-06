c5d7f2acf9cd79fd68653ef925ff8e0e97cff976
devulogi <devulogijrp.github@gmail.com>
2022-10-07 04:09:55 +0800

    applied prettier formatting

--- controllers/signup.controller.js
+++ controllers/signup.controller.js
@@ -1,37 +1,37 @@
-const User = require('../models/user.model');
-const {authenticateUser} = require('../helper');
+const User = require("../models/user.model");
+const { authenticateUser } = require("../helper");
 
 const getSignUpController = (req, res) => {
-    res.render('signup', {title: 'Sign Up'});
-}
+  res.render("signup", { title: "Sign Up" });
+};
 
 const postSignUpController = async (req, res, next) => {
-    let user; // user object
+  let user; // user object
 
-    // check if passwords match
-    if (req.body.password !== req.body.password2) {
-        res.render('signup', {title: 'Sign Up', error: 'Passwords do not match'});
-    }
+  // check if passwords match
+  if (req.body.password !== req.body.password2) {
+    res.render("signup", { title: "Sign Up", error: "Passwords do not match" });
+  }
 
-    try {
-        // check if user already exists
-        user = await User.findOne({username: req.body.username});
-        if (user) {
-            req.flash('error', 'User already exists.');
-            res.redirect('/signup');
-        } else {
-            user = new User(req.body); // create new user object
-            await user.save(); // save user to database
-            req.flash('info', 'You have successfully signed up.');
-            // authenticate user and redirect to home page
-            authenticateUser(req, res, next);
-        }
-    } catch (error) {
-        return next(error);
+  try {
+    // check if user already exists
+    user = await User.findOne({ username: req.body.username });
+    if (user) {
+      req.flash("error", "User already exists.");
+      res.redirect("/signup");
+    } else {
+      user = new User(req.body); // create new user object
+      await user.save(); // save user to database
+      req.flash("success", "You have successfully signed up.");
+      // authenticate user and redirect to home page
+      authenticateUser(true)(req, res, next);
     }
-}
+  } catch (error) {
+    return next(error);
+  }
+};
 
 module.exports = {
-    getSignUpController,
-    postSignUpController
-}
+  getSignUpController,
+  postSignUpController,
+};
