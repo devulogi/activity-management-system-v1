33aaf13c5d3f1a30af3451f90d1ce0ad42091797
devulogi <devulogijrp.github@gmail.com>
2022-10-11 03:48:55 +0800

    refactor: moved passport implementation its own service

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const path = require("path");
 const session = require("express-session");
 const MemoryStore = require("memorystore")(session);
 const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
 const { connect: db, connection } = require("mongoose").default;
 const flash = require("connect-flash");
+require("dotenv").config();
 
 const app = express();
 const routes = require("./routes");
-const User = require("./models/user.model");
+const passportService = require("./services/passport.service");
 
 // set up pug for templating
 app.set("view engine", "pug");
@@ -24,7 +24,7 @@ app.use(morgan("dev")); // log every request to the console
 app.use(express.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(
   session({
-    secret: "a2V5Ym9hcmQgY2F0", // used to sign the session ID cookie
+    secret: process.env.EXPRESS_SESSION_SECRET, // used to sign the session ID cookie
     resave: false, // don't save session if unmodified
     saveUninitialized: true, // don't create session until something stored
     cookie: {
@@ -46,57 +46,16 @@ app.use(
   })
 ); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
-passport.serializeUser(function (user, cb) {
-  cb(null, user._id);
-}); // store the user id in the session555
-passport.deserializeUser(function (id, cb) {
-  User.findById(id, function (err, user) {
-    if (err) {
-      return cb(err);
-    }
-    cb(null, user); // return the user object
-  });
-}); // retrieve the user object from the session
-passport.use(
-  "local",
-  new LocalStrategy(
-    {
-      usernameField: "username",
-      passwordField: "password",
-    },
-    async (username, password, done) => {
-      try {
-        const user = await User.findOne({ username: username });
-        // if user is not found
-        if (!user) {
-          return done(null, false, { message: "Incorrect username." });
-        } else {
-          // compare passwords
-          user.comparePassword(password, (err, isMatch) => {
-            if (err) {
-              return done(err);
-            } else if (!isMatch) {
-              return done(null, false, { message: "Incorrect password." });
-            } else {
-              return done(null, user);
-            }
-          });
-        }
-      } catch (err) {
-        return done(err);
-      }
-    }
-  )
-);
+passportService(); // configure passport strategies and serialize/deserialize user
 
 // Routes
 app.use(routes);
 
 // Start server
-app.listen(3000, () => {
+app.listen(process.env.EXPRESS_APP_PORT, () => {
   console.log("Server started on port 3000");
   // Connect to database
-  db("mongodb://localhost:27017/express-auth", {
+  db(`${process.env.MONGODB_URI}/${process.env.MONGODB_DB}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     connectTimeoutMS: 1000,
